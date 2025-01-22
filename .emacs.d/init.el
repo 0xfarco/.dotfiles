df6166e5072e2da3839c6b2882107888c0138a62
0xfarco <0xfarco@gmail.com>
2025-01-22 22:20:38 +0530

    helm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -137,6 +137,28 @@
 (setq dired-listing-switches "-alh")
 (setq dired-mouse-drag-files t)
 
+;;; helm
+(use-package helm
+	:ensure t)
+(use-package helm-git-grep
+	:ensure t)
+(use-package helm-ls-git
+	:ensure t)
+
+(setq helm-ff-transformer-show-only-basename nil)
+
+(global-set-key (kbd "C-c h t") 'helm-cmd-t)
+(global-set-key (kbd "C-c h g") 'helm-git-grep)
+(global-set-key (kbd "C-c h l") 'helm-ls-git-ls)
+(global-set-key (kbd "C-c h f") 'helm-find)
+(global-set-key (kbd "C-c h a") 'helm-org-agenda-files-headings)
+(global-set-key (kbd "C-c h r") 'helm-recentf)
+;; Invoke `helm-git-grep' from isearch.
+;; (define-key isearch-mode-map (kbd "C-c h g") 'helm-git-grep-from-isearch)
+;; Invoke `helm-git-grep' from other helm.
+;; (eval-after-load 'helm
+;;   '(define-key helm-map (kbd "C-c h g") 'helm-git-grep-from-helm))
+
 ;;; yasnippet
 (use-package yasnippet
 	:ensure t)
