435d1974dfdddccef161aff843b8632775f42384
0xfarco <0xfarco@gmail.com>
2025-02-13 20:22:00 +0530

    disable autosave for tramp buffers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -182,6 +182,10 @@
 
 (yas-global-mode 1)
 
+;;; tramp
+;;; http://stackoverflow.com/questions/13794433/how-to-disable-autosave-for-tramp-buffers-in-emacs
+(setq tramp-auto-save-directory "/tmp")
+
 ;;; powershell
 (use-package powershell
 	:ensure t)
