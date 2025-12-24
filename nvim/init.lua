2ad79e0c1ca9c6f26ead2ee9b9e36892eac3676b
0xfarco <105166593+0xfarco@users.noreply.github.com>
2025-12-24 06:19:00 +0000

    comment out nvim-treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,7 +27,7 @@ local themes = {
 require("lazy").setup({
   require(themes[env_var_nvim_theme]),
   require 'plugins.telescope',
-  require 'plugins.treesitter',
+  -- require 'plugins.treesitter',
   require 'plugins.lsp',
   require 'plugins.autocompletion',
   -- require 'plugins.none-ls',
