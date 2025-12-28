b7dc26085184c39d817b8dd1445a05a272d4aa41
Far Han Cholakkal <0xfarco@gmail.com>
2025-12-28 14:22:31 +0530

    change font to Iosevka

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,7 +10,7 @@
 (defun rc/get-default-font ()
   (cond
    ((eq system-type 'windows-nt) "Consolas-13")
-   ((eq system-type 'gnu/linux) "JetBrainsMono Nerd Font-12")))
+   ((eq system-type 'gnu/linux) "Iosevka-11")))
 
 (add-to-list 'default-frame-alist `(font . ,(rc/get-default-font)))
 
