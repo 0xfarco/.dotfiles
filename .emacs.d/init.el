9dc4db22d87d49bfc46e2e7d8888f77e9899ce40
Far Han Cholakkal <0xfarco@gmail.com>
2026-01-12 11:04:10 +0530

    update keybindings for scaling text

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,6 +28,8 @@
 
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
 
 (global-set-key (kbd "s-<left>")  #'shrink-window-horizontally)
 (global-set-key (kbd "s-<right>") #'enlarge-window-horizontally)
